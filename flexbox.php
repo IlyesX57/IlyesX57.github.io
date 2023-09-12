be114f3e52fc14159de215d740957e84e1ed6263
IlyesX57 <baylan.ilyes2.0@gmail.com>
2023-09-12 13:27:53 +0200

    Maj

--- flexbox.php
+++ flexbox.php
@@ -10,6 +10,32 @@
             <div class= 'rouge'></div>
             <div class="vert"></div>
             <div class= 'orange'></div>
+            <div class="vert"></div>
+            <div class="rouge"></div>
+            <div class="orange"></div>    
+            <div class="vert"></div>
+            <div class="rouge"></div>
+            <div class="orange"></div>  
+            <div class="vert"></div>
+            <div class="rouge"></div>
+            <div class="orange"></div>  
+
+    </div>
+    <br><br>
+
+    <div class="grand">
+        <div class="petit">
+            <h1>FlexBox</h1>
+        </div>
+        <div class="petit" id="secondpetit">
+            <div class="moyen"></div>
+            <div class="toutpetit"></div>
+            <div class="toutpetit"></div>
+            <div class="moyen"></div>
+        </div>
+    </div>
+<br><br>
+    <div class="exercice">
 
 
        
