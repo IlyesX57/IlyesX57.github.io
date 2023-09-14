4d2a08ba48d4d8295275dc08a8c57c1375375fc2
IlyesX57 <baylan.ilyes2.0@gmail.com>
2023-09-14 11:54:32 +0200

    Maj

--- clock.php
+++ clock.php
@@ -3,67 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <title>Horloge</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- faire un minuteur numérique-->
-    <!-- on doit pouvoir changer 
-l'heure avec un formulaire  -->
-<p id="minuteur">00:00:00</p>
-<form action=""method="post">
-<label for="heure">Heure(s) :</label>
-<input type="number" name="heure" id="heure" value=0 min=0 max=23 lenght=2> 
+    <p id="minuteur">00:00:00</p>
+    <form action="" method="post">
+        <label for="heure">Heure(s) :</label>
+        <input type="number" name="heure" id="heure" value=0 min=0>
 
-<label for="minute">minute(s) :</label>
-<input type="number" name="minute" id="minute" value=0 min=0 max=59>
+        <label for="minute">Minute(s) :</label>
+        <input type="number" name="minute" id="minute" value=0 min=0 max=59 lenght=2>
 
-<label for="seconde">seconde(s) :</label>
-<input type="number" name="seconde" id="seconde" value=0 min=0 max=59>
-<input type="submit" value="Régler">
-<button type="button" id="pause">Pause</button>
-<button type="button" id="play">Play</button>
+        <label for="seconde">Seconde(s) :</label>
+        <input type="number" name="seconde" id="seconde" value=0 min=0 max=59 lenght=2>
 
-</form>
+        <input type="submit" value="Régler">    
+        <button type="button" id="pause" onclick="PauseTimer()">Pause</button>
+        <button type="button" id="play">Play</button>
+    </form>
 
 
+    <?php 
+        echo '<script>
+        var heure = ' . (!empty($_POST["heure"]) ? $_POST["heure"] : '0') . '
+        var minute = ' . (!empty($_POST["minute"]) ? $_POST["minute"] : '0') . '
+        var seconde = ' . (!empty($_POST["seconde"]) ? $_POST["seconde"] : '0') . '
+        var interval = setInterval(function() {            
+            document.getElementById("minuteur").innerHTML= 
+            `${(heure < 10 ? "0" : "") + heure}:${(minute < 10 ? "0" : "") + minute}:${(seconde < 10 ? "0" : "") + seconde}`
+            if (seconde <= 0 && minute <= 0 && heure <= 0 ) return
 
-<?php
-
-
-echo '<script>
-var heure = '.(!empty($_POST["heure"]) ? $_POST["heure"] : '0').'
-var minute = '.(!empty($_POST["minute"]) ? $_POST_["minute"] : '0').'
-var seconde = '.(!empty($_POST["seconde"]) ? $_POST_["seconde"] : '0').'
-var Interval = setInterval (function() {
-   document.getElementById("minuteur").innerHTML=
-    `${(heure < 10 ? "0":"") + heure}:${(minute < 10 ? "0":"") + minute}:${(seconde < 10 ? "0":"") + seconde}`
-    if (seconde <= 0) return
-    seconde--
-    if (seconde == 0){
-        if (minute > 0) {
-            seconde = 59
-            minute--
-        } else { 
-            if (heure > 0) {
-                heure--
-                minute = 59
-                seconde = 59
+            if (seconde == 0) {
+                if (minute > 0) {
+                    seconde = 60
+                    minute--
+                } else {
+                    if (heure > 0) {
+                        heure--
+                        minute = 59
+                        seconde = 60
+                    }
+                }
             }
+            seconde--
+        },  1000)
+        function PauseTimer() {
+            clearInterval(interval)
+            document.getElementById("minuteur").style.color = "red"
         }
-    }
-}, 1000)
-function PauseTimer() {
-    clearInterval(Interval)
-    document.getElementById("minuteur").style.color =red
-}
-
-</script>';
-
-?>
-
-
-
-
+    </script>';
+    ?>
+    <!-- Faite un minuteur numérique 
+    On doit pouvoir changer le temps qu'il dur
+     avec un formulaire -->
 </body>
-
 </html>
