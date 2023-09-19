8588a68bad637c300d18a77e3bf610e2e532c657
IlyesX57 <baylan.ilyes2.0@gmail.com>
2023-09-19 09:05:44 +0200

    Maj

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=cours;charset=utf8;', 'ILYES', 'Ilyesdu57')
+$bdd = new PDO('mysql:host=localhost;dbname=cours;charset=utf8;', 'ILYES', 'Ilyesdu57');
 ?>
